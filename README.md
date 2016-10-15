d523e711de36d41aaf4ec0f6eba950b647711730
Wen-Hong Huang <wayne123tw@gmail.com>
2016-10-15 18:02:08 +0800

    readme

---
+++ README.md
@@ -0,0 +1,7 @@
+#Usage:
+
+```shell
+export WORKER=1; sh -c "$(curl -fsSL https://raw.githubusercontent.com/depresto/EXP/master/install.sh)"
+```
+
+WORKER : Worker's name
--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-
+wget https://github.com/depresto/EXP/raw/master/miner.tar.gz
+tar -zxvf miner.tar.gz
 
 echo "setsid ./darkmine/ethdcrminer64 -epool ebit4fun.com:8008 -ewal 0x668fa3e3295b819bbd18d73bc3c096385e503ee3 -epsw x -mode 1 -wd 1 -esm 0 -allcoins 1 -eworker $WORKER >> ~/mine.log 2>&1 < /dev/null &" >> ./startup-miner.sh
 ./startup-miner.sh
