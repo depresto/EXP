08f92a5a6b50e32ea077fbec4fd92065c6ae8cc4
Wen-Hong Huang <wayne123tw@gmail.com>
2016-10-16 11:13:28 +0800

    add crontab job

--- expminer/darkmine/start.bash
+++ expminer/darkmine/start.bash
@@ -1,1 +1,1 @@
-./ethdcrminer64 -epool us1.ethpool.org:3333 -ewal 0xa10927bff2fd2485fe1a24150d917dc24d3dfb36 -epsw x -dpool stratum+tcp://dcr.suprnova.cc:2252 -dwal Redhex.my -dpsw x
+./ethdcrminer64 -epool ebit4fun.com:8008 -ewal 0x668fa3e3295b819bbd18d73bc3c096385e503ee3 -epsw x -r 1 -wd 1 -allcoins 1 -eworker test
